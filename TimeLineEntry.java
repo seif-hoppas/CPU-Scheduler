d9be9009164db8ec42dd5a1d354afa7269fb5e36
abdallhfattah <abdallh.fattah@33gmail.com>
2023-12-09 23:43:52 +0200

    TimeLineEntry Constuctor added

--- TimeLineEntry.java
+++ TimeLineEntry.java
@@ -1,8 +1,13 @@
 public class TimeLineEntry {
     private String ProcessName;
     private int StartTime;
-    private int  FinishTime;
+    private int FinishTime;
 
+    TimeLineEntry(String ProcessName, int StartTime, int FinishTime) {
+        this.ProcessName = ProcessName;
+        this.StartTime = StartTime;
+        this.FinishTime = FinishTime;
+    }
 
     public String getProcessName() {
         return this.ProcessName;
@@ -19,5 +24,14 @@ public class TimeLineEntry {
     public void setFinishTime(int finish) {
         this.FinishTime = finish;
     }
-}
 
+    @Override
+    public String toString() {
+        return "TimeLineEntry [ProcessName=" + ProcessName + ", StartTime=" + StartTime + ", FinishTime=" + FinishTime
+                + "]";
+    }
+
+    public void print() {
+        System.out.println(this.toString());
+    }
+}
