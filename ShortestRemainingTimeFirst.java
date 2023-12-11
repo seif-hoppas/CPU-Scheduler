f5bd995ac4a80ba6e9fffc0936c706d8d4bd22cf
abdallhfattah <abdallh.fattah@33gmail.com>
2023-12-11 09:44:40 +0200

    updated TimeLine for the process to take at least 1 unit of time

--- ShortestRemainingTimeFirst.java
+++ ShortestRemainingTimeFirst.java
@@ -4,8 +4,8 @@ import java.util.PriorityQueue;
 public class ShortestRemainingTimeFirst extends CPUScheduler {
     // Time Line
     /*
-     * p0   p0
-     *   p1
+     * p0 p0
+     * p1
      */
 
     PriorityQueue<ProcessControlBlock> ReadyQueue;
@@ -22,8 +22,8 @@ public class ShortestRemainingTimeFirst extends CPUScheduler {
     /*
      * 1 2 3 4 5 6 7 8 9 --->
      * - - - - 1 1 1 1 1 1
-     *             2 2
-     *                  3 3 3 3
+     * 2 2
+     * 3 3 3 3
      */
 
     @Override
@@ -32,7 +32,8 @@ public class ShortestRemainingTimeFirst extends CPUScheduler {
         while (!ReadyQueue.isEmpty() || !ProcessControlBlockList.isEmpty()) {
 
             // submit a new process to the Ready Queue
-            while (!ProcessControlBlockList.isEmpty() && ProcessControlBlockList.get(0).getArrivalTime() <= currentTime) {
+            while (!ProcessControlBlockList.isEmpty()
+                    && ProcessControlBlockList.get(0).getArrivalTime() <= currentTime) {
                 // remove from list , add to readyqueue
                 ReadyQueue.add(ProcessControlBlockList.remove(0));
             }
@@ -42,25 +43,26 @@ public class ShortestRemainingTimeFirst extends CPUScheduler {
                 CurrentProcessControlBlock = ReadyQueue.poll();
             }
 
-            if(CurrentProcessControlBlock != null){
+            if (CurrentProcessControlBlock != null) {
                 System.out.println("Time " + currentTime + ": Running process " + CurrentProcessControlBlock.getName());
 
                 // Add very process to execute in the time line
-                if(Timeline.size() == 0){
-                    Timeline.add(new TimeLineEntry(CurrentProcessControlBlock.getName() , currentTime, currentTime + 1));
+                if (Timeline.size() == 0) {
+                    Timeline.add(new TimeLineEntry(CurrentProcessControlBlock.getName(), currentTime, currentTime + 1));
                 }
 
-                // a new process to be added into the Timeline 
-                else if(Timeline.size() != 0 && CurrentProcessControlBlock.getName() != Timeline.get(Timeline.size() - 1).getProcessName()){
-                    Timeline.add(new TimeLineEntry(CurrentProcessControlBlock.getName() , currentTime, currentTime + 1));
+                // a new process to be added into the Timeline
+                else if (Timeline.size() != 0
+                        && CurrentProcessControlBlock.getName() != Timeline.get(Timeline.size() - 1).getProcessName()) {
+                    Timeline.add(new TimeLineEntry(CurrentProcessControlBlock.getName(), currentTime, currentTime + 1));
                 }
-                // the process still executing then update its finish time 
-                else{
+                // the process still executing then update its finish time
+                else {
                     Timeline.get(Timeline.size() - 1).setFinishTime(currentTime);
                 }
 
                 CurrentProcessControlBlock.setBurstTime(CurrentProcessControlBlock.getBurstTime() - 1);
-                if(CurrentProcessControlBlock.getBurstTime() != 0){
+                if (CurrentProcessControlBlock.getBurstTime() != 0) {
                     ReadyQueue.add(CurrentProcessControlBlock);
                 }
             }
